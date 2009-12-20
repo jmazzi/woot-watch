ad576921426b836acf151e608d70ab348232a9a8
Justin Mazzi <hh@mailheist.com>
2009-12-19 23:54:31 -0500

    Adding my other domain

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def google_analytics
-    render(:partial => 'layouts/google_analytics') if request.host == 'woot.heroku.com'
+    if ['woot.heroku.com', 'woot.r00tshell.com'].include?(request.host)
+      render(:partial => 'layouts/google_analytics') 
+    end
   end
 end
