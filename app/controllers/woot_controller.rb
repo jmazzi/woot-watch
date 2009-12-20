e5f43387eb9803c4a4f766dd26344b15229939a2
Justin Mazzi <hh@mailheist.com>
2009-12-19 20:48:07 -0500

    General cleanups

--- app/controllers/woot_controller.rb
+++ app/controllers/woot_controller.rb
@@ -8,15 +8,5 @@ class WootController < ApplicationController
     unless @woot = read_fragment('woot', :expires_in => 10.seconds) || write_fragment('woot', Woot.parse)
       expire_fragment('woot')
     end
-    respond_to do |format|
-      format.html
-      format.js do
-        render :update do |page|
-          page.replace_html  'product', :partial => 'product'
-          page.visual_effect :highlight, 'product'
-        end
-      end
-    end
   end
-
 end
--- app/helpers/woot_helper.rb
+++ app/helpers/woot_helper.rb
@@ -1,7 +1,12 @@
 module WootHelper
-  def refresh_link
-    link_to_remote('Refresh', :url => { :action => "update"}, 
-                    :loading => "$('refresh').toggle();$('loading').toggle();",
-                    :complete => "$('loading').toggle();$('refresh').toggle();")
+  def link_to_refresh
+    link_to_remote('Refresh', 
+      :url => { :action => "update"}, 
+      :loading => "$('refresh').toggle();$('loading').toggle();",
+      :complete => "$('loading').toggle();$('refresh').toggle();")
+  end
+  
+  def refresh_page(last_item)
+    periodically_call_remote(:url => { :action => 'update', :last_item => last_item })
   end
 end
