ca60cc06608ead317277bf668dd633f03bb00c9a
Justin Mazzi <hh@mailheist.com>
2009-12-26 01:06:39 -0500

    Turn off caching

--- app/controllers/woot_controller.rb
+++ app/controllers/woot_controller.rb
@@ -4,8 +4,9 @@ class WootController < ApplicationController
   end
 
   def update
-    unless @woot = read_fragment('woot', :expires_in => 10.seconds) || write_fragment('woot', Woot.parse)
-      expire_fragment('woot')
-    end
+    #unless @woot = read_fragment('woot', :expires_in => 10.seconds) || write_fragment('woot', Woot.parse)
+    #  expire_fragment('woot')
+    #end
+    @woot = Woot.parse
   end
 end
